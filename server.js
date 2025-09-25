6d41a0f86bae606b28bb17888291b6e34bd2ec91
Fuad Mohammed <fuadmohammed368@gmail.com>
2025-09-25 17:50:50 +0300

    add more security middleware

--- server.js
+++ server.js
@@ -2,18 +2,53 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import productRoute from './routes/productRoute.js'
+import userRoute from './routes/userRoute.js'
+import xss from 'xss-clean'
+import helmet from 'helmet'
+import cors from 'cors'
+import rateLimit from 'express-rate-limit'
+import mongoSanitize from 'express-mongo-sanitize'
+
+
 dotenv.config()
 
 const app = express()
-
+//
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// cors for cross origin requests
+app.use(cors())
+//data sanitization against DOS attacks
+app.use(xss())
+app.use(helmet())
+app.use(mongoSanitize())
+
+//rate limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again later."
+});
+app.use(limiter);
 
 app.get('/' , (req, res)=>{
     res.send("API is running...")
 })
 
+// Routes
+app.use('api/products/', productRoute)
+app.use('/api/users/', userRoute)
+// app.use('/api/orders/', orderRoute)
+// app.use('/api/categories/', categoryRoute)
 
-app.use('/api/products', productRoute)
+//error handling middleware
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+})
 
 
 mongoose.connect(process.env.MONGODB_URI)
