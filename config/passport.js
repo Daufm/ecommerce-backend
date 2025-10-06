481f973b3bb470dbd070042048c5962ea166bb99
Fuad Mohammed <fuadmohammed368@gmail.com>
2025-10-06 23:23:59 +0300

    impliment passport and callbackurl

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ import User from "../Model/User.js";
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "api/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
@@ -14,11 +14,15 @@ passport.use(new GoogleStrategy({
       let user = await User.findOne({ email: profile.emails[0].value });
       if (!user) {
         // If not, create a new user
-        user = new User({
+         user = new User({
           name: profile.displayName,
           email: profile.emails[0].value,
           passwordHash: "", // No password for OAuth users
-          googleId: profile.id
+          googleId: profile.id,
+          roles : ["customer"],
+          isEmailVerified: true,
+          provider: "google",
+          
         });
         await user.save();
       }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,6 +15,13 @@ router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
     // Successful authentication
+    const token = jwt.sign({ id: req.user._id, roles: req.user.roles }, process.env.JWT_SECRET, { expiresIn: '20m' });
+    // You can set the token in a cookie or return it in the response
+    res.cookie('jwt', 
+              token, 
+            { httpOnly: true, 
+              secure: process.env.NODE_ENV === 'production' }); 
+ 
     // res.redirect('/'); // Redirect to your desired route
     res.json({ message: "Google OAuth successful", user: req.user });
   }
